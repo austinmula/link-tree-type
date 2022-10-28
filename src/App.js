0ef1b4eb6d0ea675e3c6b7430b2b79ecf16dde11
austinmula <maustin64.ma@gmail.com>
2022-10-29 01:10:01 +0300

    basic layout and styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">
+        <main>
+          <div className="container-main">
+            <div className="profile-section"></div>
+            <div className="links">
+              <a href="http://">Twitter</a>
+              <a href="http://">Twitter</a>
+              <a href="http://">Twitter</a>
+              <a href="http://">Twitter</a>
+              <a href="http://">Twitter</a>
+              <a href="http://">Twitter</a>
+            </div>
+          </div>
+          <div className="container-socials">socials</div>
+        </main>
+        <footer>footer</footer>
+      </div>
+    </>
   );
 }
 
